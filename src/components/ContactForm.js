d9904d658b43693d568c2063e55d83bec1f075d1
Deprasny <deprasny@gmail.com>
2020-09-23 20:48:21 +0700

    inisiasi

---
+++ src/components/ContactForm.js
@@ -0,0 +1,16 @@
+import React,{useState,useEffect} from "react";
+
+const ContactForm = () => {
+const initialFieldValues ={
+    fullname:'',
+    mobile:'',
+    email:'',
+    address:''
+}
+    var []
+    return ( 
+        <div>Display Form</div> 
+     );
+}
+ 
+export default ContactForm;
---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyD7zBwNZULG2IenNII2IFh6-wyQZLk6pAM",
+    authDomain: "react-crud-83b26.firebaseapp.com",
+    databaseURL: "https://react-crud-83b26.firebaseio.com",
+    projectId: "react-crud-83b26",
+    storageBucket: "react-crud-83b26.appspot.com",
+    messagingSenderId: "809806638556",
+    appId: "1:809806638556:web:fe8635c02ad4266f843319"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
